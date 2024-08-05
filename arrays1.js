1cd898a3cce73a6aa4dad73f9d28bfd9bddf2cbb
unknown <eng.Khaled10@outlook.com>
2024-08-05 20:41:13 +0300

    hi

---
+++ arrays1.js
@@ -0,0 +1,9 @@
+let groceries = ["food", "drink", "books", "elc", "toys", "casher"];
+console.log(groceries[1]);
+console.log(groceries.length);
+console.log(groceries[5]);
+groceries.pop();
+console.log(groceries[groceries.length - 1]);
+console.log(groceries);
+groceries.push("first add", "second add");
+console.log(groceries);
